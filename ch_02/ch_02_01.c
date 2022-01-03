6867d71a57579ed65e6eb597777b95b2ad03599e
Darbaz Ali <darbaz.me@gmail.com>
2022-01-03 21:14:18 +0300

    first C program

---
+++ ch_02/ch_02_01.c
@@ -0,0 +1,21 @@
+/* 
+    C && C++ - How to program 
+    @ Chapter 1
+    @ program: ch_02_01
+    @ file: welcome.c
+    @ auther: Darbaz Ali
+    @ date: Jan 3, 2022
+ */
+
+// Print a simple message to the terminal
+
+// include preproccessor directive
+#include <stdio.h>
+
+// main function, each C program must contain main function to begin executing the code
+int main(void)
+{
+
+    // print a message
+    printf("Welcome to C!\n");
+} // end of main function
