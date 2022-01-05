b4f571cb4b08e8c7bfcc61d20284bac64c87710c
Darbaz Ali <darbaz.me@gmail.com>
2022-01-05 22:16:47 +0300

    review exercises

---
+++ ch_02/ch_02_04.c
@@ -0,0 +1,24 @@
+/* 
+    Self review excercises
+
+    @file: Excercises
+    @author: Darbaz Ali
+    @date: Jan 5, 2022
+
+ */
+
+ #include <stdio.h>
+
+ int main (void) {
+    // 1) Define 4 vairables of type int
+    int a, b, c, d;
+
+    // 2) prompt a user to enter a number
+    printf("Enter a number: ");
+
+    // 3) read a value from keyboard
+    scanf("%d", &a);
+
+ }
+
+
