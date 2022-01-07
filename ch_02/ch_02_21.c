9be65fc692fce9ca7f5e81ed59a5a5620ddc1d2b
Darbaz Ali <darbaz.me@gmail.com>
2022-01-07 21:06:27 +0300

    Shapes with Asterisks

---
+++ ch_02/ch_02_21.c
@@ -0,0 +1,18 @@
+/* 
+    Shapes with Asterisks
+
+    @file: ch_02_21
+    @author: Darbaz Ali
+    @date: Jan 7, 2022
+
+ */
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+    printf("*********\t\t***\t\t* \n");
+    printf("*\t*\t*\t*\t***\t* * \n");
+
+    printf("*********\t\t***\t\t* \n");
+}
