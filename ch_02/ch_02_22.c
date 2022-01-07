3f6161802e46c87dc8d257036db4eacecf2cbae3
Darbaz Ali <darbaz.me@gmail.com>
2022-01-07 21:12:51 +0300

    Trangle shape with Asterisks

---
+++ ch_02/ch_02_22.c
@@ -0,0 +1,15 @@
+/* 
+    Triangular shape with Asterisks
+
+    @file: ch_02_22
+    @author: Darbaz Ali
+    @date: Jan 7, 2022
+
+ */
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+    printf("*\n**\n***\n****\n*****\n******\n*******\n********\n*********\n***********\n");
+}
