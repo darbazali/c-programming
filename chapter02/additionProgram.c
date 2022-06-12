1e2dfde0475c5a36f21d452cb7a461b8828cd93f
Darbaz Ali <darbaz.me@gmail.com>
2022-06-12 23:06:45 +0300

    addition program

--- chapter02/additionProgram.c
+++ chapter02/additionProgram.c
@@ -1,34 +1,21 @@
-// Fig 2.5
+// Figure 2.3
 // Addition Program
-// @desc: Reads 2 integers from standard input and adds these 2 integer together
-// @author: Darbaz Ali
-// @date: May 10th, 2022
-
-// include stdio header
 #include <stdio.h>
 
 // function main begins program execution
 int main(void)
 {
+    int integer1; // first number to be enterd by user
+    int integer2; // second number to be enterd by user
 
-    int integer1; // first integer to be entered by user
-    int integer2; // second integer to be entered by user
-
-    // prompt user to enter first number
-    printf("Enter the first number: \n");
+    printf("Enter first integer \n"); // prompt
     scanf("%d", &integer1); // read an integer
 
-    // prompt user to enter second number
-    printf("Enter the second number: \n");
-    scanf("%d", &integer2); // read an integer
+    printf("Enter second integer \n"); // prompt
+    scanf("%d", &integer2);
 
-    // variable sum to save the addition of both numbers
-    int sum;
-
-    // add integer1 on to integer2 and put the result inside sum variable
+    int sum; // the variable in which sum will be stored
     sum = integer1 + integer2;
 
-    // termination phase
-    printf("The sum is %d \n", sum);
-
-} // end of main
+    printf("The sum is %d \n", sum); // print sum 
+} // end of function main
