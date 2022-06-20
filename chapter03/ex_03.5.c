5d23b246274ab3e4635dabb28491b34c81a7c127
Darbaz Ali <darbaz.me@gmail.com>
2022-06-20 22:47:38 +0300

    sum of range of integers

---
+++ chapter03/ex_03.5.c
@@ -0,0 +1,20 @@
+// Calcluate the sum of integers from 1 to 10
+#include <stdio.h>
+
+// function main begins program execution
+int main(void){
+	
+	// eclaration phase
+	unsigned int counter = 1;
+	unsigned int sum = 0;
+	
+	// proccessing phase
+	while ( counter <= 10) {
+		sum += 1;
+		counter++;
+	} // end while
+	
+	// termination phase
+	printf("Counter is %d \n", counter);
+} // end function main
+
---
+++ tests.c
@@ -0,0 +1,11 @@
+// This file is for testing snipets
+#include <stdio.h>
+
+int main(void)
+{
+	int count = 20;
+	
+	if (count > 10) puts("Count is grater than 10");
+	
+	printf("%.2f", 123.4567);
+}
